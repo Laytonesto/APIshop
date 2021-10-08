7152a635c2711e7eac71f9422ff20bbdc9275071
Laytonesto <ejcf.1997@gmail.com>
2021-10-07 20:44:43 -0600

    Web Creada, Consumo de API

--- WEBshop/Controllers/HomeController.cs
+++ WEBshop/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using APIshop.Models.Procedures;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +13,12 @@ namespace WEBshop.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("http://localhost/APIshop/api/Productos/Get");
+            var productosList = JsonConvert.DeserializeObject<List<get_productos>>(json);
+            return View(productosList);
         }
 
         public ActionResult About()
@@ -25,6 +33,45 @@ namespace WEBshop.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public async Task<ActionResult> Login(usuario usuario)
+        {
+            var response = string.Empty;
+
+            var payload = "{\"correo\": \"" + usuario.correo+ "\",\"password\": \"" + usuario.password+ "\"}";
+            HttpContent c = new StringContent(payload, Encoding.UTF8, "application/json");
+            var httpClient = new HttpClient();
+            var result = await httpClient.PostAsync(new Uri("http://localhost/APIshop/api/Usuarios/Login"), c );
+            
+
+            if (result.IsSuccessStatusCode)
+            {
+                response = result.StatusCode.ToString();
+            }
+
+            return View();
+        }
+
+        public async Task<ActionResult> Registro(usuario usuario)
+        {
+            var response = string.Empty;
+
+            var payload = "{\"nombre\": \"" + usuario.nombre + "\",\"genero\": \"" + usuario.genero + "\",\"correo\": \"" + usuario.correo + "\",\"password\": \"" + usuario.password + "\"}";
+            HttpContent c = new StringContent(payload, Encoding.UTF8, "application/json");
+            var httpClient = new HttpClient();
+            var result = await httpClient.PostAsync(new Uri("http://localhost/APIshop/api/Usuarios/Registro"), c);
+
+
+            if (result.IsSuccessStatusCode)
+            {
+                response = result.StatusCode.ToString();
+            }
+            var loginList = JsonConvert.DeserializeObject<List<usuario>>(response);
+
+            return View();
+
+
         }
     }
 }
